84d1df00dd5745b84d2d528a0c5fdf22aa052072
syntax@live.dk <syntax@live.dk@cbb44350-1988-11df-8728-136f2c2ca76e>
2010-02-21 11:33:38 +0000

    Done with retrieveUserDatas. Updated the db too.

---
+++ trunk/Server Source/Game/Users/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AQWE.Game.Managers;
+
+namespace AQWE.Game.Users
+{
+    public class Item
+    {
+        internal int ID;
+        internal int ItemID;
+        internal int UserID;
+        internal bool Equipped;
+
+        public Item(int _ID)
+        {
+            this.ID = _ID;
+            userItemManager.Add(this.ID, this);
+        }
+    }
+}
